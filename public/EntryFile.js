8f74e2e4887785f13fed5b9e2d5e06cd25b5eb7b
Keshav <nknnkumar@yahoo.com>
2024-06-02 13:35:44 +0530

    keshav

--- public/EntryFile.js
+++ public/EntryFile.js
@@ -4,6 +4,9 @@ import menu from './menu.json' assert { type: "json"};
 import nunjucks from "nunjucks";
 import fs from "fs-extra";
 import path from "path";
+import { existsSync, mkdirSync } from 'fs';
+
+const toFolderName = "bin";
 
 var env = nunjucks.configure();
 
@@ -16,39 +19,59 @@ env.addGlobal('startsWith', startsWith);
 
 let html = fs.readFileSync("Normal.html", 'utf8');
 
-menu.forEach(menuItem => {
-    if ("children" in menuItem) {
-        menuItem.children.forEach(child => {
-            let LocalFileName = ("template" in child) ? child.template : "Normal.html";
-            let html = fs.readFileSync(`Template/${LocalFileName}`, 'utf8');
+const LoopMenu = (inMenuJson) => {
+    inMenuJson.forEach(menuItem => {
+        if ("children" in menuItem) {
+            menuItem.children.forEach(child => {
+                let LocalFileName = ("template" in child) ? child.template : "Normal.html";
+                let html = fs.readFileSync(`Template/${LocalFileName}`, 'utf8');
 
-            let data = nunjucks.renderString(html,
-                { menu, menuItem: child}
-            );
-            // let folder = child.url.slice(0, -5);
-            let folder = path.parse(child.url).name;
+                let data = nunjucks.renderString(html,
+                    { menu, menuItem: child }
+                );
+                // let folder = child.url.slice(0, -5);
+                let folder = path.parse(child.url).name;
 
-            if (!fs.existsSync(`bin/${folder}`)) fs.mkdirSync(`bin/${folder}`);
-            fs.writeFileSync(`bin/${folder}/${child.url}`, data);
-        });
-    }
-    else {
-        if ("template" in menuItem) {
-            html = fs.readFileSync(`Template/${menuItem.template}`, 'utf8');
+                if (!fs.existsSync(`bin/${folder}`)) fs.mkdirSync(`bin/${folder}`);
+                fs.writeFileSync(`bin/${folder}/${child.url}`, data);
+            });
         }
         else {
-            html = fs.readFileSync("Normal.html", 'utf8');
+            if ("template" in menuItem) {
+                html = fs.readFileSync(`Template/${menuItem.template}`, 'utf8');
+            }
+            else {
+                html = fs.readFileSync("Normal.html", 'utf8');
+            };
+
+            let data = nunjucks.renderString(html,
+                { menu, menuItem }
+            );
+            let folder = menuItem.url.slice(0, -5);
+
+            if (!fs.existsSync(`
+        /${folder}`)) fs.mkdirSync(`bin/${folder}`);
+            // fs.mkdirSync(`bin/${folder}`);
+            fs.writeFileSync(`bin/${folder}/${menuItem.url}`, data);
         };
+    });
+};
 
-        let data = nunjucks.renderString(html,
-            { menu, menuItem }
-        );
-        let folder = menuItem.url.slice(0, -5);
+const createDirIfNotExists = dir =>
+    !existsSync(dir) ? mkdirSync(dir) : undefined;
 
-        if (!fs.existsSync(`bin/${folder}`)) fs.mkdirSync(`bin/${folder}`);
-        // fs.mkdirSync(`bin/${folder}`);
-        fs.writeFileSync(`bin/${folder}/${menuItem.url}`, data);
+let StartFunc = () => {
+    if (existsSync(toFolderName)) {
+        console.log("Delete the bin folder first!");
+        return;
     };
-});
 
+    mkdirSync(toFolderName);
+
+    LoopMenu(menu);
+
+    console.log(`Inserted to the bin folder : ${menu.length} menu items`);
+};
+
+StartFunc();
 // console.log(menu);

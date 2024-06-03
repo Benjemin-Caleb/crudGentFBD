3657dc65e3ac9ea8fb57e009da3b9216fe00cbce
Ch-Sai-Jaswanth <saijaswanthch2003@gmail.com>
2024-06-03 13:14:20 +0530

    Multi chat changes

--- public/KWebSocket/SPA/js/FormLoad/Addlisteners/MultiChatId/ClickFunc/EntryFile.js
+++ public/KWebSocket/SPA/js/FormLoad/Addlisteners/MultiChatId/ClickFunc/EntryFile.js
@@ -1,17 +1,5 @@
-import { StartFunc as StartFuncAddListeners } from "./AddListeners/EntryFile.js";
-import { StartFunc as StartFuncBuildBSTable } from "./BuildBsTable/EntryFile.js";
-
 let StartFunc = () => {
-  let temp = document.getElementById("templateMultiChat");
-  let clon = temp.content.cloneNode(true);
-  let replace = document.getElementById("SectionId");
-  document.getElementById("pageId").replaceChild(clon, replace);
-  document.getElementById("headId").style.display = "none";
-  replace.style.display = "none";
-  document.getElementById("breadCrumpId").style.display = "none";
-
-  StartFuncBuildBSTable();
-  // StartFuncAddListeners();
-};
+  document.getElementById("pageId").innerHTML = "";
+}
 
 export { StartFunc };

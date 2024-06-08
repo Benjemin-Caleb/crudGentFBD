19ff47433eba979936786c746df85900b1337f92
keshavsoft <nknnkumar@yahoo.com>
2024-06-09 01:41:36 +0530

    keshav

--- public/Template/Js/Show/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/PostFetch.js
+++ public/Template/Js/Show/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/PostFetch.js
@@ -1,7 +1,9 @@
-import ConfigJson from '../../../../Config.json' with {type: 'json'};
+// import ConfigJson from '../../../../Config.json' with {type: 'json'};
 
 let StartFunc = async () => {
-    let jVarLocalFetchUrl = `/bin/${ConfigJson.tableName}/DataOnly`;
+    let jVarLocalTableName=jVarGlobalTableName;
+
+    let jVarLocalFetchUrl = `/bin/${jVarLocalTableName}/DataOnly`;
     let response = await fetch(jVarLocalFetchUrl);
     
     return await response;
--- public/Template/Js/Show/FormLoad/BuildBsTable/EntryFile.js
+++ public/Template/Js/Show/FormLoad/BuildBsTable/EntryFile.js
@@ -1,20 +1,42 @@
 import { StartFunc as StartFuncTableTag } from "./TableTag.js";
-import ConfigJson from `/bin/config/${jVarGlobalTableName}` with {type: 'json'};
+//import ConfigJson from `/bin/config/${jVarGlobalTableName}` with {type: 'json'};
 // import ConfigJson from '/bin/config/Openings.json' with {type: 'json'};
-
 
 const StartFunc = () => {
     StartFuncTableTag();
-    jFLocalInitialize();
+    jFLocalInitialize().then();
 };
 
-const jFLocalInitialize = () => {
+const jFLocalInitialize = async () => {
     var $table = $('#table');
+    let jVarLocalData = await jFLocalReturnColumns();
 
+    console.log("jVarLocalData : ", jVarLocalData);
     $table.bootstrapTable({
         data: [],
-        columns:ConfigJson.tableConfig.columns
+        columns:jVarLocalData
     });
+};
+
+const jFLocalReturnColumns = async () => {
+    let jVarLocalData = await jFLocalTableSchema();
+    let jVarLocalColumns=Object.keys(jVarLocalData);
+
+  let jVarLocalCollection=  jVarLocalColumns.map(element => {
+        return{
+            field:element,
+            title:element
+        }
+    });
+
+ return   jVarLocalCollection;
+};
+
+const jFLocalTableSchema = async () => {
+    let jVarLocalResponse = await fetch(`/321/${jVarGlobalTableName}.json`);
+    let jVarLocalData = await jVarLocalResponse.json();
+
+    return await jVarLocalData;
 };
 
 export { StartFunc };
